f5652243ecf5200f7dc98755750165cd028f4a56
Cheng Li <scrappedprince.li@gmail.com>
2015-11-10 12:30:06 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+# -*- coding: utf-8 -*-
+u"""
+Created on 2015-11-10
+
+@author: cheng.li
+"""
+
 from distutils.core import setup
 
 setup(
     name='VisualPortfolio',
     version='0.1.0',
     packages=['VisualPortfolio'],
+    py_modules=['VisualPortfolio.__init__'],
     url='',
     license='',
     author='cheng.li',
